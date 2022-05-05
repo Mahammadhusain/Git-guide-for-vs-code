a7f2b1ad38fb4c2d7b152b0ce511b62d27ab8eb5
Mahammadhusain <kadiwala.530@gmail.com>
2022-05-05 18:30:22 +0530

    first commit

---
+++ git.py
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------------------------------------------
+# -------------------- Git-Hub with Vs Code ----------------------------------------------------------- 
+# -----------------------------------------------------------------------------------------------------
+
+# --------------  Requirement ----------------------
+    # ==> Need install Git
+    # ==> Also Need Account
+
+# ---------------- Pull request -----------------------
+# pull request is used for make changes in other user repository if other user merged with pull request then
+# he get changes in his repository 
+
+
+# ---------------- Create new branch with master branch -----------------------
+# go view----> command pelette ----> Git:Create New Branch  ===== for create new branch
+# go view----> command pelette ----> Git:Checkout to  ===== for switch one branch to another
+# go view----> command pelette ----> Git:Delete Branch  ===== for delete branch
+
+# ---------------- get updated code form Github to local sytem  -----------------------
+# select three dots and select (Pull) option ==== > u get updated code which was merged with (version1) to (master) branch in you local system
+
+
+
